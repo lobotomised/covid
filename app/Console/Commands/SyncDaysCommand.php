612c3f50377780d13f0e804c8ede931f06f18b67
lobotomised <romain@rprevost.fr>
2020-07-06 14:45:25 +0200

    empty response

--- app/Console/Commands/SyncDaysCommand.php
+++ app/Console/Commands/SyncDaysCommand.php
@@ -31,17 +31,17 @@ class SyncDaysCommand extends Command
         }
 
         $confirmed_data = json_decode($confirmed, true);
-        if ($confirmed_data) {
+        if ($confirmed_data && json_last_error() === JSON_ERROR_NONE) {
             $this->sync('confirmed', $confirmed_data);
         }
 
         $deaths_data = json_decode($deaths, true);
-        if ($deaths_data) {
+        if ($deaths_data && json_last_error() === JSON_ERROR_NONE) {
             $this->sync('deaths', $deaths_data);
         }
 
         $recovered_data = json_decode($recovered, true);
-        if ($recovered_data) {
+        if ($recovered_data && json_last_error() === JSON_ERROR_NONE) {
             $this->sync('recovered', $recovered_data);
         }
     }
