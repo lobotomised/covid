2d708ff9138e8c9c2c903c153648c81dfafdb0b1
lobotomised <romain@rprevost.fr>
2020-10-06 20:00:39 +0200

    update test without helper

--- tests/Unit/DeltaTest.php
+++ tests/Unit/DeltaTest.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use App\Support\Number;
 
 class DeltaTest extends TestCase
 {
     /** @test **/
     public function it_can_get_a_negative_delta(): void
     {
-        $output = delta(10, 15);
+        $output = Number::delta(10, 15);
 
         $this->assertEquals('-5',  $output);
     }
@@ -19,7 +20,7 @@ class DeltaTest extends TestCase
     /** @test **/
     public function it_can_get_a_null_delta(): void
     {
-        $output = delta(10, 10);
+        $output = Number::delta(10, 10);
 
         $this->assertEquals('0',  $output);
     }
@@ -27,7 +28,7 @@ class DeltaTest extends TestCase
     /** @test **/
     public function it_can_get_a_positive_delta(): void
     {
-        $output = delta(15, 10);
+        $output = Number::delta(15, 10);
 
         $this->assertEquals('+5',  $output);
     }
