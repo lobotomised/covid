9902b3b45a1fcd4ac0b515a965c3f29c8226c90c
lobotomised <34192441+lobotomised@users.noreply.github.com>
2020-10-14 09:51:30 +0200

    missing dash

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -1,5 +1,5 @@
 Situation pour le {{ $date->format('d/m/Y') }}
 
-Touchés: {{ number_format($confirmed, 0, ',', ' ') }} ({{ $confirmedDelta }})
+Touchés: {{ number_format($confirmed, 0, ',', ' ') }} - ({{ $confirmedDelta }})
 Morts: {{ number_format($deaths, 0, ',', ' ') }} - ({{ $deathsDelta }})
 Rétablies: {{ number_format($recovered, 0, ',', ' ') }} - ({{ $recoveredDelta }})
